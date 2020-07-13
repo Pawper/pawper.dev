b92cdbd5e3957cd4016e8a96ceef9fb792465ea6
Phillip A. Wessels <phillip.a.wessels@gmail.com>
2020-07-12 21:47:44 -0700

    Scroll fix for mobile

--- pad/js/index.js
+++ pad/js/index.js
@@ -5,14 +5,14 @@ console.log(location.hash);
 console.log(location.hash.length);
 
 if (location.hash.length !== "") {
-  console.log('Currently at ' + currentSection)
+  console.log('Currently at ' + currentSection);
   for(let section = 0; section < contentSections.length; section++) {
     if (contentSections[section].id !== currentSection.substr(1)) {
-      contentSections[section].classList.add('closed')
-      console.log('Closed ' + contentSections[section].id)
+      contentSections[section].classList.add('closed');
+      console.log('Closed ' + contentSections[section].id);
     } else {
-      contentSections[section].classList.remove('closed')
-      console.log('Opened ' + contentSections[section].id)
+      contentSections[section].classList.remove('closed');
+      console.log('Opened ' + contentSections[section].id);
     }
   }
 }
@@ -21,16 +21,16 @@ for(let button = 0; button < navButtons.length; button++) {
   navButtons[button].addEventListener('click', function() {
     console.log('click');
     for(let section = 0; section < contentSections.length; section++) {
-      contentSections[section].classList.add('closed')
-      console.log('Closed ' + contentSections[section].id)
+      contentSections[section].classList.add('closed');
+      console.log('Closed ' + contentSections[section].id);
     }
     for(let section = 0; section < contentSections.length; section++) {
       if (contentSections[section].id == navButtons[button].getAttribute("href").substr(1)) {
-        contentSections[section].classList.remove('closed')
+        contentSections[section].classList.remove('closed');
         contentSections[section].scrollTop = 0;
-        window.scrollTop = 0;
-        console.log('Opened ' + contentSections[section].id)
+        window.scrollTo(0, 0);
+        console.log('Opened ' + contentSections[section].id);
       }
     }
-  })
+  });
 }
