120518417a7e8e460a0b9130810f394dec236081
Pawper <phillip.a.wessels@gmail.com>
2021-11-16 23:48:37 -0800

    Removing img imports in JS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import './sass/main.scss'
-import './img/product.png'
-import './img/pwessels.jpg'
-import './img/survey.png'
-import './img/tribute.png'
 
 if (module.hot) {
   module.hot.accept();
